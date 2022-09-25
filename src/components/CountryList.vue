7cc55185a10d28a87d855f83c8489915ce3b4bef
LarsMoons <lars.moons@student.uantwerpen.be>
2022-09-25 18:46:28 +0200

    reorder home images

--- src/components/CountryList.vue
+++ src/components/CountryList.vue
@@ -56,29 +56,28 @@ import CountryItem from "@/components/CountryItem.vue";
 
     <CountryItem>
       <template #title>
-        Balearen
+        Noorwegen
       </template>
 
       <template #image>
-        <img class="image" src="@/assets/home/balearen.jpg"/>
+        <img class="image" src="@/assets/home/noorwegen.jpg"/>
       </template>
 
       <template #content>
-        <p>Een eigen historische ontwikkeling en de verschillen in reliëf en neerslag maken elk eiland weer anders.
-          Majorca, het grootste eiland, heeft de meest gevarieerde natuur. De hoofdplaats Palma getuigt van een
-          welvarend
-          verleden. Menorca heeft een uitgesproken landelijk karakter met imposante overblijfselen uit de verre
-          prehistorie: taula's, talayot's en naveta's... Ibiza, het witte eiland, met een schril contrast tussen drukke
-          toeristische stranden en de rust van het platteland. Formentera: stranden, een molen... je bezoekt het in 1
-          dag... met de fiets.</p>
+        <p>Het zuiden van Noorwegen is een gebied met bergen en hoogplateaus, met dalen en met diepe fjorden. We trekken
+          door Jotunheimen en langs de Sognefjord, waar de mooiste stavkerkjes staan. We bezoeken de steden Oslo en
+          Bergen, het pittoreske stadje Stavanger, de art nouveau-stad Ålesund en Trondheim, de poort tot
+          Noord-Noorwegen,
+          het land van de middernachtzon. De Lofoten zijn eilanden met idyllische vissershaventjes. Tromsø was de
+          thuishaven van de poolreiziger Roald Amundsen. Hammerfest is de meest noordelijke stad van Noorwegen. Karasjok
+          en Kautokeino zijn de belangrijkste Sami-nederzettingen. Het "einde van de weg" bereiken we een eerste maal
+          voorbij Kirkenes, aan de Russische grens. Een tweede maal eindigt de weg op de klippen van de Noordkaap.</p>
       </template>
 
       <template #link>
-        <router-link class="country-link" to="/balearen">Meer</router-link>
+        <router-link class="country-link" to="/noorwegen">Meer</router-link>
       </template>
     </CountryItem>
-
-
 
     <CountryItem>
       <template #title>
@@ -105,54 +104,26 @@ import CountryItem from "@/components/CountryItem.vue";
 
     <CountryItem>
       <template #title>
-        Kreta Santorini
+        IJsland
       </template>
 
       <template #image>
-        <img class="image" src="@/assets/home/kretasantorini.jpg"/>
+        <img class="image" src="@/assets/home/ijsland-2.jpg"/>
       </template>
 
       <template #content>
-        <p>De Griekse mythologie getuigt van grote vindingrijkheid en verbeeldingskracht. De ontdekking van het paleis
-          te
-          Knossos in het begin van de 20ste eeuw heeft de mythe dichter bij de werkelijkheid gebracht. In het eerste
-          deel
-          verkennen we Kreta en Santorini met de geschiedenis als rode draad. Grieken, Romeinen, Moren, Byzantijnen,
-          Venetianen, Turken en Duitsers: ze lieten er allemaal hun sporen na.
-
-          In het tweede deel gaat onze aandacht naar de natuur, het cultureel erfgoed en de hedendaagse samenleving. De
-          Witte Bergen hebben de hoogste toppen en de diepste ravijnen. Het gebied met de Samariakloof is beschermd als
-          Nationaal Park. Slechts een vierde van het land is geschikt voor landbouw. Kreta produceert olijfolie, wijn,
-          citrusvruchten en tomaten; toch is toerisme nu de belangrijkste bron van inkomsten.</p>
+        <p>De kredietcrisis en vooral de aswolk van de vulkaan Eyjafjallajökull, hebben IJsland in het nieuws gebracht.
+          De
+          plaats waar de Noorman Ingolfur Arnarson in 874 zijn boerderij optrok, groeide uit tot de hoofdstad,
+          Reykjavik.
+          Daar woont nu zowat de helft van de totale bevolking. Het binnenland daarentegen is desolaat en nagenoeg
+          onbewoond. Het "land van ijs" heeft uitgestrekte gletsjers, meren vol ijsbergen, woeste rivieren en machtige
+          watervallen. Het "land van vuur" is bezaaid met vulkanen, kratermeren, geisers, solfataren, warme bronnen en
+          stoomvelden. IJslanders zijn fier op hun taal die in de loop der eeuwen slechts weinig is veranderd</p>
       </template>
 
       <template #link>
-        <router-link class="country-link" to="/kreta-santorini">Meer</router-link>
-      </template>
-    </CountryItem>
-
-    <CountryItem>
-      <template #title>
-        Noorwegen
-      </template>
-
-      <template #image>
-        <img class="image" src="@/assets/home/noorwegen.jpg"/>
-      </template>
-
-      <template #content>
-        <p>Het zuiden van Noorwegen is een gebied met bergen en hoogplateaus, met dalen en met diepe fjorden. We trekken
-          door Jotunheimen en langs de Sognefjord, waar de mooiste stavkerkjes staan. We bezoeken de steden Oslo en
-          Bergen, het pittoreske stadje Stavanger, de art nouveau-stad Ålesund en Trondheim, de poort tot
-          Noord-Noorwegen,
-          het land van de middernachtzon. De Lofoten zijn eilanden met idyllische vissershaventjes. Tromsø was de
-          thuishaven van de poolreiziger Roald Amundsen. Hammerfest is de meest noordelijke stad van Noorwegen. Karasjok
-          en Kautokeino zijn de belangrijkste Sami-nederzettingen. Het "einde van de weg" bereiken we een eerste maal
-          voorbij Kirkenes, aan de Russische grens. Een tweede maal eindigt de weg op de klippen van de Noordkaap.</p>
-      </template>
-
-      <template #link>
-        <router-link class="country-link" to="/noorwegen">Meer</router-link>
+        <router-link class="country-link" to="/ijsland">Meer</router-link>
       </template>
     </CountryItem>
 
@@ -186,57 +157,53 @@ import CountryItem from "@/components/CountryItem.vue";
 
     <CountryItem>
       <template #title>
-        Suriname
+        Kreta Santorini
       </template>
 
       <template #image>
-        <img class="image" src="@/assets/home/suriname.jpg"/>
+        <img class="image" src="@/assets/home/kretasantorini.jpg"/>
       </template>
 
       <template #content>
-        <p>Suriname, Zuid-Amerika, bijna op de evenaar, en toch: hier spreekt men Nederlands !!! Het half miljoen
-          Surinamers woont vooral in de kuststrook en in de hoofdstad Paramaribo, UNESCO Werelderfgoed. Suriname is
-          beslist een rijk land… althans wat de bodem betreft. Die is rijk aan goud, bauxiet en petroleum. De geladen
-          geschiedenis bracht een multi-etnische bevolking voort: Indianen, Boscreolen, Chinezen, Hindoestanen, Javanen…
-          Boeiend zijn een bezoek aan de kleine dorpen in het Amazonewoud, een excursie naar de oude plantages en naar
-          de
-          stranden waar grote zeeschildpadden hun eieren komen leggen.</p>
+        <p>De Griekse mythologie getuigt van grote vindingrijkheid en verbeeldingskracht. De ontdekking van het paleis
+          te
+          Knossos in het begin van de 20ste eeuw heeft de mythe dichter bij de werkelijkheid gebracht. In het eerste
+          deel
+          verkennen we Kreta en Santorini met de geschiedenis als rode draad. Grieken, Romeinen, Moren, Byzantijnen,
+          Venetianen, Turken en Duitsers: ze lieten er allemaal hun sporen na.
+
+          In het tweede deel gaat onze aandacht naar de natuur, het cultureel erfgoed en de hedendaagse samenleving. De
+          Witte Bergen hebben de hoogste toppen en de diepste ravijnen. Het gebied met de Samariakloof is beschermd als
+          Nationaal Park. Slechts een vierde van het land is geschikt voor landbouw. Kreta produceert olijfolie, wijn,
+          citrusvruchten en tomaten; toch is toerisme nu de belangrijkste bron van inkomsten.</p>
       </template>
 
       <template #link>
-        <router-link class="country-link" to="/suriname">Meer</router-link>
+        <router-link class="country-link" to="/kreta-santorini">Meer</router-link>
       </template>
     </CountryItem>
 
     <CountryItem>
       <template #title>
-        Taiwan
+        Balearen
       </template>
 
       <template #image>
-        <img class="image" src="@/assets/home/taiwan-2.jpg"/>
+        <img class="image" src="@/assets/home/balearen.jpg"/>
       </template>
 
       <template #content>
-        <p>In de zestiende eeuw ontdekten Portugese zeevaarders een mooi eiland en noemden het “Ilha Formosa”, het
-          huidige
-          TAIWAN.
-          De hoofdstad TAIPEI is een grootstad met hoge torengebouwen, brede lanen, ruime parken en pleinen, zeer druk
-          verkeer maar met een modern en vlot transportsysteem. Het is eveneens een Chinese stad, met schitterende
-          gebouwen in de traditionele stijl van de Chinese paleizen, met kleurrijke tempels vol bedrijvigheid en rook
-          van
-          de vele offerstokjes.
-          <br><br>
-          Taiwan heeft prachtige natuurgebieden en nationale parken. Het park Yangmingshan ligt ten noorden van Taipei
-          en
-          Kenting Nationaal Park in het uiterste zuiden van het eiland. Niet te missen is het Taroko Nationaal Park aan
-          de
-          oostkust, een rivierkloof in een prachtig berglandschap.
-        </p>
+        <p>Een eigen historische ontwikkeling en de verschillen in reliëf en neerslag maken elk eiland weer anders.
+          Majorca, het grootste eiland, heeft de meest gevarieerde natuur. De hoofdplaats Palma getuigt van een
+          welvarend
+          verleden. Menorca heeft een uitgesproken landelijk karakter met imposante overblijfselen uit de verre
+          prehistorie: taula's, talayot's en naveta's... Ibiza, het witte eiland, met een schril contrast tussen drukke
+          toeristische stranden en de rust van het platteland. Formentera: stranden, een molen... je bezoekt het in 1
+          dag... met de fiets.</p>
       </template>
 
       <template #link>
-        <router-link class="country-link" to="/taiwan">Meer</router-link>
+        <router-link class="country-link" to="/balearen">Meer</router-link>
       </template>
     </CountryItem>
 
@@ -270,80 +237,33 @@ import CountryItem from "@/components/CountryItem.vue";
 
     <CountryItem>
       <template #title>
-        China
+        Taiwan
       </template>
 
       <template #image>
-        <img class="image" src="@/assets/home/china-2.jpg"/>
+        <img class="image" src="@/assets/home/taiwan-2.jpg"/>
       </template>
 
       <template #content>
-        <p>China, de gesloten wereld van weleer, is resoluut de snelweg van de modernisering ingeslagen. Op veel
-          plaatsen
-          dreigt de sfeer van het traditionele China te verdwijnen. Eerst verkennen we drie belangrijke steden:
-          Shanghai,
-          Beijing, en Xi’an, wereldberoemd omwille van het Terracotta leger. We bezoeken onderweg de cultuurhistorisch
-          belangrijke plaatsen en hebben oog voor de natuur. Daarna bezoeken we het zuidelijk deel van China met de
-          etnische minderheden, Tibet, sites met UNESCO Werelderfgoed, bekende natuurmonumenten en tot slot Macao en
-          Hong
-          Kong.</p>
+        <p>In de zestiende eeuw ontdekten Portugese zeevaarders een mooi eiland en noemden het “Ilha Formosa”, het
+          huidige
+          TAIWAN.
+          De hoofdstad TAIPEI is een grootstad met hoge torengebouwen, brede lanen, ruime parken en pleinen, zeer druk
+          verkeer maar met een modern en vlot transportsysteem. Het is eveneens een Chinese stad, met schitterende
+          gebouwen in de traditionele stijl van de Chinese paleizen, met kleurrijke tempels vol bedrijvigheid en rook
+          van
+          de vele offerstokjes.
+          <br><br>
+          Taiwan heeft prachtige natuurgebieden en nationale parken. Het park Yangmingshan ligt ten noorden van Taipei
+          en
+          Kenting Nationaal Park in het uiterste zuiden van het eiland. Niet te missen is het Taroko Nationaal Park aan
+          de
+          oostkust, een rivierkloof in een prachtig berglandschap.
+        </p>
       </template>
 
       <template #link>
-        <router-link class="country-link" to="/china">Meer</router-link>
-      </template>
-    </CountryItem>
-
-    <CountryItem>
-      <template #title>
-        IJsland
-      </template>
-
-      <template #image>
-        <img class="image" src="@/assets/home/ijsland-2.jpg"/>
-      </template>
-
-      <template #content>
-        <p>De kredietcrisis en vooral de aswolk van de vulkaan Eyjafjallajökull, hebben IJsland in het nieuws gebracht.
-          De
-          plaats waar de Noorman Ingolfur Arnarson in 874 zijn boerderij optrok, groeide uit tot de hoofdstad,
-          Reykjavik.
-          Daar woont nu zowat de helft van de totale bevolking. Het binnenland daarentegen is desolaat en nagenoeg
-          onbewoond. Het "land van ijs" heeft uitgestrekte gletsjers, meren vol ijsbergen, woeste rivieren en machtige
-          watervallen. Het "land van vuur" is bezaaid met vulkanen, kratermeren, geisers, solfataren, warme bronnen en
-          stoomvelden. IJslanders zijn fier op hun taal die in de loop der eeuwen slechts weinig is veranderd</p>
-      </template>
-
-      <template #link>
-        <router-link class="country-link" to="/ijsland">Meer</router-link>
-      </template>
-    </CountryItem>
-
-    <CountryItem>
-      <template #title>
-        Madeira
-      </template>
-
-      <template #image>
-        <img class="image" src="@/assets/home/madeira-2.jpg"/>
-      </template>
-
-      <template #content>
-        <p>MADEIRA, parel in de Atlantische Oceaan, drijvende tuin, bloemenparadijs... Deze omschrijvingen dankt het
-          eiland aan zijn mild klimaat. Madeira werd een druk centrum voor de handel in suiker en kleurstoffen.
-          Vlaanderen
-          was een belangrijke handelspartner. Musea en kerken bezitten Vlaamse kunstwerken uit de 16de eeuw. Op de
-          berghellingen zijn terrassen aangelegd voor de landbouw. Een uitgebreid netwerk van kanalen of levada’s, zorgt
-          voor de bevloeiing. Toerisme is nu de grootste bron van inkomsten voor Madeira. Het is een paradijs voor
-          wandelaars. De tocht van de Pico Ruivo naar de Pico do Arieiro is een topper.
-
-          Veertig kilometer ten noordoosten van Madeira, ligt er een kleiner eiland: PORTO-SANTO. Het gouden zand en het
-          blauwe water maken van dit eiland een paradijs voor zonnekloppers.</p>
-
-      </template>
-
-      <template #link>
-        <router-link class="country-link" to="/madeira">Meer</router-link>
+        <router-link class="country-link" to="/taiwan">Meer</router-link>
       </template>
     </CountryItem>
 
@@ -397,6 +317,87 @@ import CountryItem from "@/components/CountryItem.vue";
       </template>
     </CountryItem>
 
+    <CountryItem>
+      <template #title>
+        Madeira
+      </template>
+
+      <template #image>
+        <img class="image" src="@/assets/home/madeira-2.jpg"/>
+      </template>
+
+      <template #content>
+        <p>MADEIRA, parel in de Atlantische Oceaan, drijvende tuin, bloemenparadijs... Deze omschrijvingen dankt het
+          eiland aan zijn mild klimaat. Madeira werd een druk centrum voor de handel in suiker en kleurstoffen.
+          Vlaanderen
+          was een belangrijke handelspartner. Musea en kerken bezitten Vlaamse kunstwerken uit de 16de eeuw. Op de
+          berghellingen zijn terrassen aangelegd voor de landbouw. Een uitgebreid netwerk van kanalen of levada’s, zorgt
+          voor de bevloeiing. Toerisme is nu de grootste bron van inkomsten voor Madeira. Het is een paradijs voor
+          wandelaars. De tocht van de Pico Ruivo naar de Pico do Arieiro is een topper.
+
+          Veertig kilometer ten noordoosten van Madeira, ligt er een kleiner eiland: PORTO-SANTO. Het gouden zand en het
+          blauwe water maken van dit eiland een paradijs voor zonnekloppers.</p>
+
+      </template>
+
+      <template #link>
+        <router-link class="country-link" to="/madeira">Meer</router-link>
+      </template>
+    </CountryItem>
+
+    <CountryItem>
+      <template #title>
+        Suriname
+      </template>
+
+      <template #image>
+        <img class="image" src="@/assets/home/suriname.jpg"/>
+      </template>
+
+      <template #content>
+        <p>Suriname, Zuid-Amerika, bijna op de evenaar, en toch: hier spreekt men Nederlands !!! Het half miljoen
+          Surinamers woont vooral in de kuststrook en in de hoofdstad Paramaribo, UNESCO Werelderfgoed. Suriname is
+          beslist een rijk land… althans wat de bodem betreft. Die is rijk aan goud, bauxiet en petroleum. De geladen
+          geschiedenis bracht een multi-etnische bevolking voort: Indianen, Boscreolen, Chinezen, Hindoestanen, Javanen…
+          Boeiend zijn een bezoek aan de kleine dorpen in het Amazonewoud, een excursie naar de oude plantages en naar
+          de
+          stranden waar grote zeeschildpadden hun eieren komen leggen.</p>
+      </template>
+
+      <template #link>
+        <router-link class="country-link" to="/suriname">Meer</router-link>
+      </template>
+    </CountryItem>
+
+
+
+
+
+    <CountryItem>
+      <template #title>
+        China
+      </template>
+
+      <template #image>
+        <img class="image" src="@/assets/home/china-2.jpg"/>
+      </template>
+
+      <template #content>
+        <p>China, de gesloten wereld van weleer, is resoluut de snelweg van de modernisering ingeslagen. Op veel
+          plaatsen
+          dreigt de sfeer van het traditionele China te verdwijnen. Eerst verkennen we drie belangrijke steden:
+          Shanghai,
+          Beijing, en Xi’an, wereldberoemd omwille van het Terracotta leger. We bezoeken onderweg de cultuurhistorisch
+          belangrijke plaatsen en hebben oog voor de natuur. Daarna bezoeken we het zuidelijk deel van China met de
+          etnische minderheden, Tibet, sites met UNESCO Werelderfgoed, bekende natuurmonumenten en tot slot Macao en
+          Hong
+          Kong.</p>
+      </template>
+
+      <template #link>
+        <router-link class="country-link" to="/china">Meer</router-link>
+      </template>
+    </CountryItem>
 
   </div>
 </template>
